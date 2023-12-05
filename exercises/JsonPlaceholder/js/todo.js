4b462ee746b9b2f633bea0c0f3cd2e7940948106
J Gdovic <jgdovic3@gmail.com>
2023-12-05 13:22:50 -0500

    todo page is done working  user info being display

--- exercises/JsonPlaceholder/js/todo.js
+++ exercises/JsonPlaceholder/js/todo.js
@@ -1,15 +1,14 @@
 "use strict";
 let toDoTextbox = document.getElementById("toDoTextbox");
 let addToDoButton = document.getElementById("addToDoButton");
-let toDoParagraph = document.getElementById("toDoParagraph");
+let toDoDiv = document.getElementById("toDoDiv");
 
 function addTodo() {
   let toDoID = toDoTextbox.value;
   fetch("https://jsonplaceholder.typicode.com/todos/" + toDoID)
     .then((response) => response.json())
     .then((data) => {
-      let message = `To Do: ${data.title} `;
-      toDoParagraph.innerText = message;
+      toDoDiv.innerText = `To Do: ${data.title} `;
     });
 }
 addToDoButton.onclick = addTodo;
