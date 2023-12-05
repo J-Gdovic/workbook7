d0eb4f3ea36906157b4c21684ce42e71cf34a936
J Gdovic <jgdovic3@gmail.com>
2023-12-05 11:23:57 -0500

    to do excersise

---
+++ exercises/JsonPlaceholder/js/todo.js
@@ -0,0 +1,22 @@
+"use strict";
+let toDoTextbox = document.getElementById("toDoTextbox");
+let addToDoButton = document.getElementById("addToDoButton");
+let toDoParagraph = document.getElementById("toDoParagraph");
+
+
+function addTodo(){
+    let toDoID = toDoTextbox.value
+    fetch("https://jsonplaceholder.typicode.com/todos/"+ toDoID)
+    .then(response=> response.json())
+    .then((data) => {
+        let message = `To Do: ${data.title} `;
+        toDoParagraph.innerText = message;
+    });
+       
+    
+   
+}
+addToDoButton.onclick = addTodo;
+
+
+
