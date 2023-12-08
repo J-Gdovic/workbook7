705fc020a66b3a30b4c5e488a1d1a7a62699c4b4
J Gdovic <jgdovic3@gmail.com>
2023-12-08 15:27:56 -0500

    cant get products in drop down

--- workshop/script/search.js
+++ workshop/script/search.js
@@ -1,11 +1,37 @@
 "use strict";
 let productSelect = document.querySelector("#productSelect");
-let categorySelect = document.querySelector("#categorySelect");
-let veiwAllSelect = document.querySelector("#veiwAllSelect");
 
-fetch("http://localhost:8081/api/categories")
-  .then((response) => response.json())
-  .then((categoryArray) => {
-    // console.log(categoryArray);
-    return categoryArray;
-  });
+function fetchProductData() {
+  //fetch by view all products
+  fetch("http://localhost:8081/api/products")
+    .then((response) => response.json())
+    .then((productArray) => {
+      // console.log(productArray);
+      return productArray;
+    });
+}
+fetchProductData();
+
+function loadProductDataDropDown(productArray) {
+    for (const product of productArray){
+        let option = document.createElement("option");
+        option.value = product.productId;
+        option.innerText = product.productName;
+        productSelect.appendChild(option);
+    }
+   
+}
+loadProductDataDropDown();
+
+
+
+//////////////////////////////////////////////////////////
+function loadCategoryData() {
+  //fetch by categories
+  fetch("http://localhost:8081/api/categories")
+    .then((response) => response.json())
+    .then((categoryArray) => {
+      // console.log(categoryArray);
+      return categoryArray;
+    });
+}
