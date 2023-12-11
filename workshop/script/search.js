af1f611603ea974f66c31e6e1f143df730e25687
J Gdovic <jgdovic3@gmail.com>
2023-12-11 09:33:12 -0500

    loading product/category info

--- workshop/script/search.js
+++ workshop/script/search.js
@@ -10,28 +10,41 @@ function fetchProductData() {
       return productArray;
     });
 }
-fetchProductData();
 
-function loadProductDataDropDown(productArray) {
-    for (const product of productArray){
-        let option = document.createElement("option");
-        option.value = product.productId;
-        option.innerText = product.productName;
-        productSelect.appendChild(option);
-    }
-   
+
+function loadProductDataDropDown() {
+  for (const product of productArray) {
+    let option = document.createElement("option");
+    option.value = product.productId;
+    option.innerText = product.productName;
+    productSelect.appendChild(option);
+  }
 }
-loadProductDataDropDown();
-
 
 
 //////////////////////////////////////////////////////////
-function loadCategoryData() {
+function fetchCategoryData() {
   //fetch by categories
   fetch("http://localhost:8081/api/categories")
     .then((response) => response.json())
     .then((categoryArray) => {
-      // console.log(categoryArray);
-      return categoryArray;
+      console.log(categoryArray);
+      // return categoryArray;
     });
 }
+
+
+function loadCategoryDataDropDown() {
+  for (const category of categoryArray) {
+    let option = document.createElement("option");
+    option.value = category.categorytId;
+    option.innerText = category.name;
+    productSelect.appendChild(option);
+  }
+}
+
+/////////////wire Up
+fetchProductData(); //fetches the product array
+loadProductDataDropDown(); // loads the product name in dropdown
+fetchCategoryData();// fetches the category array
+loadCategoryDataDropDown() // loads the category name into a dropdown
